a98ded40aeef11ba880a4da5ac7b124199e73020
Jenna McLaughlin <89791791+jennagmclaughlin@users.noreply.github.com>
2026-03-15 03:25:56 -0400

    local storage, layout fixing

---
+++ src/js/app.js
@@ -0,0 +1,69 @@
+const zipInput = document.getElementById("zipInput");
+const submitBtn = document.getElementById("zipCodeSubmit");
+const zipContain = document.body.querySelector("#zipcode .container");
+const plantsContain = document.body.querySelector("#plants-list .container");
+
+const user = JSON.parse(localStorage.getItem("userInfo"));
+
+async function grabZip(zip) {
+  try {
+    const response = await fetch(`https://phzmapi.org/${zip}.json`);
+    const zoneResults = await response.json();
+    return zoneResults;
+  } catch (error) {
+    console.error(error);
+    zipContain.innerHTML = "";
+    plantsContain.innerHTML = "";
+    window.alert(error);
+    // don't clear local storage for errors in case user messes up
+    zipInput.classList.add("errorInput");
+    display();
+    return null;
+  }
+}
+
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const zipCodeRegex = /^\d{5}$/;
+  let zipCode = zipInput.value;
+  if (zipCodeRegex.test(zipCode)) {
+    let result = await grabZip(zipCode);
+    const userInfo = {
+      zip: zipCode,
+      zone: result.zone,
+      temp: result.temperature_range,
+      lat: result.coordinates.lat,
+      lon: result.coordinates.lon,
+      plants: [],
+    };
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    display();
+  } else {
+    zipInput.classList.add("errorInput");
+    window.alert("Please add a valid zip code! :-)");
+  }
+});
+
+function display() {
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+
+  if (user == null) {
+    zipContain.innerHTML = "<p>Please enter your zip code! :-)</p>";
+    plantsContain.innerHTML = "";
+    return;
+  }
+  zipContain.innerHTML = `
+            <h2 class="heading-xl text-center">Your hardiness zone is ${user.zone}</h2>
+            <ul class="zoneDetails font-lg text-center">
+                <li><strong>Extreme Winter Temperature Range (in Fahrenheit):</strong> ${user.temp}</li>
+                <li><strong>Latitude / Longitude:</strong> ${user.lat} / ${user.lon}</li>
+            </ul>
+        `;
+  plantsContain.innerHTML = `<p>${user.zone}</p>`;
+  zipContain.scrollIntoView();
+  zipInput.classList.remove("errorInput");
+}
+
+display();
+
+// figure out how to get plants going
