0ed9422bbb76fccce9c590e053903bf68427f0a3
Jenna McLaughlin <89791791+jennagmclaughlin@users.noreply.github.com>
2026-03-16 01:38:40 -0400

    added comments for future me

--- public/main.js
+++ public/main.js
@@ -6,8 +6,8 @@ const submitBtn = document.getElementById("locSubmit");
 const locContain = document.body.querySelector("#userLoc .container");
 const plantsContain = document.body.querySelector("#plants-list .container");
 
-let data;
-
+// this is loading json once, so i don't have to reload time and time again
+let data; // defining so i can use it in function
 async function grabStates() {
   try {
     const res = await fetch("./data/us_latlng.json");
@@ -17,18 +17,20 @@ async function grabStates() {
     window.alert(error);
   }
 }
-
 grabStates();
 
+// when dropdown for states is selected
 stateSelect.addEventListener("change", async (e) => {
   e.preventDefault();
   const state = stateSelect.value;
   const counties = data[state].counties;
   countySelect.innerHTML = "";
 
+  // fills county dropdown with counties for respective state
+  // counties are an object hence the Object.keys(). took me an hour to realize that...
   Object.keys(counties).forEach((county) => {
     const option = document.createElement("option");
-    option.value = county;
+    option.value = county; // json doesn't have unique ids or anything but eh
     option.textContent = county;
     countySelect.appendChild(option);
   });
@@ -38,8 +40,8 @@ submitBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   const state = stateSelect.value;
   const county = countySelect.value;
-  const lat = data[state].counties[county].lat;
-  const lng = data[state].counties[county].lng;
+  const lat = data[state].counties[county].lat; // latitude
+  const lng = data[state].counties[county].lng; // longitude
   const userInfo = {
     state: state,
     county: county,
@@ -52,6 +54,7 @@ submitBtn.addEventListener("click", async (e) => {
 
 async function display() {
   const user = JSON.parse(localStorage.getItem("userInfo"));
+  // just in case there isn't any ls
   if (user == null) {
     locContain.innerHTML = "";
     return;
